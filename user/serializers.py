87917b0e3aad6be90de5e46a509573e07a6a37e1
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-07-24 16:03:29 +0200

    Fix user creation

--- user/serializers.py
+++ user/serializers.py
@@ -17,20 +17,20 @@ class UserSerializer(serializers.ModelSerializer):
             }
         }
 
-        def create(self, validated_data):
-            """Create user with encrypted password"""
-            return get_user_model().objects.create_user(**validated_data)
+    def create(self, validated_data):
+        """Create user with encrypted password"""
+        return get_user_model().objects.create_user(**validated_data)
 
-        def update(self, instance, validated_data):
-            """Update user with encrypted password"""
-            passwort = validated_data.pop("password", None)
-            user = super().update(instance, validated_data)
+    def update(self, instance, validated_data):
+        """Update user with encrypted password"""
+        passwort = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
 
-            if passwort:
-                user.set_password(passwort)
-                user.save()
+        if passwort:
+            user.set_password(passwort)
+            user.save()
 
-            return user
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
