1c4e6bd17236da22a77c402b6e32c3d365cbcfff
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-07-30 17:07:38 +0200

    Fix v1 to urls

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -28,8 +28,8 @@ from theatre_service import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/theatre/", include("theatre.urls", namespace="theatre")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/v1/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/v1/user/", include("user.urls", namespace="user")),
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/v1/doc/swagger/",
